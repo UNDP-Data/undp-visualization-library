1dd90f401d67b8d9de029e34ba620592a48db7f7
Mustafa Saifee <mustafa.saifee@undp.org>
2024-09-27 14:11:06 +0300

    fix transformColumnToArray functionality

--- src/Utils/transformData/transformColumnsToArray.tsx
+++ src/Utils/transformData/transformColumnsToArray.tsx
@@ -1,14 +1,16 @@
 import { ColumnConfigurationDataType } from '../../Types';
 
-export async function transformColumnsToArray(
+export function transformColumnsToArray(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  csvData: any,
+  csvData: any[],
   columnConfig: ColumnConfigurationDataType[],
 ) {
   const dataFormatted = csvData.map((el: any) => {
     const temp = { ...el };
     columnConfig.forEach(d => {
-      temp[d.column] = temp[d.column].split(d.delimiter || ',');
+      temp[d.column] = temp[d.column]
+        ? temp[d.column].split(d.delimiter || ',').map((t: string) => t.trim())
+        : [];
     });
     return temp;
   });
