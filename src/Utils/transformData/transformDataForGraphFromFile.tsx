b81c6903df514c6c26889e34727c4988cce5064a
Mustafa Saifee <mustafa.saifee@undp.org>
2024-11-11 01:41:00 +0200

    Update transformDataForGraphFromFile.tsx

--- src/Utils/transformData/transformDataForGraphFromFile.tsx
+++ src/Utils/transformData/transformDataForGraphFromFile.tsx
@@ -1,19 +1,53 @@
-import { GraphConfigurationDataType, GraphType } from '../../Types';
-import { fetchAndParseJSON, fetchAndParseCSV } from '../fetchAndParseData';
+import {
+  ColumnConfigurationDataType,
+  FileSettingsDataType,
+  GraphConfigurationDataType,
+  GraphType,
+} from '../../Types';
+import {
+  fetchAndParseJSON,
+  fetchAndParseCSV,
+  fetchAndTransformDataFromAPI,
+  fetchAndParseMultipleDataSources,
+} from '../fetchAndParseData';
 import { transformDataForGraph } from './transformDataForGraph';
 
 export async function transformDataForGraphFromFile(
-  dataURL: string,
+  dataURL: string | FileSettingsDataType[],
   dataConfiguration: GraphConfigurationDataType[],
   graph: GraphType,
-  fileType?: string,
+  fileType?: 'csv' | 'json' | 'api',
   delimiter?: string,
+  columnsToArray?: ColumnConfigurationDataType[],
+  apiHeaders?: any,
+  apiMethod?: 'POST' | 'GET' | 'DELETE' | 'PUT',
+  apiRequestBody?: any,
+  dataTransformation?: string,
+  idColumnTitle?: string,
 ): Promise<any> {
   // Ensure the function returns a Promise
   try {
-    const fetchedData = await (fileType === 'json'
-      ? fetchAndParseJSON(dataURL)
-      : fetchAndParseCSV(dataURL, undefined, false, delimiter));
+    const fetchedData = await (typeof dataURL === 'string'
+      ? fileType === 'json'
+        ? fetchAndParseJSON(dataURL, dataTransformation, columnsToArray, false)
+        : fileType === 'api'
+        ? fetchAndTransformDataFromAPI(
+            dataURL,
+            dataTransformation,
+            apiMethod || 'GET',
+            apiHeaders,
+            apiRequestBody,
+            false,
+          )
+        : fetchAndParseCSV(
+            dataURL,
+            dataTransformation,
+            columnsToArray,
+            false,
+            delimiter,
+            true,
+          )
+      : fetchAndParseMultipleDataSources(dataURL, idColumnTitle));
 
     const formattedData = transformDataForGraph(
       fetchedData,
